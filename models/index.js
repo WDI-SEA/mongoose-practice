58d4734118632039de7d0ddfc43bdad0faab72d1
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-10 16:18:26 -0700

    made my museum model schema

--- models/index.js
+++ models/index.js
@@ -2,8 +2,11 @@
 const mongoose = require('mongoose');
 
 // TODO: Connect to Mongo database
+mongoose.connect(process.env.MONGOOSE_URL || 'mongodb://localhost:27017/hellowdi20', { useNewUrlParser: true });
 
 // TODO: Require your other models, and export them
+module.exports.Museum = require('./museum');
+module.exports.Piece = require('./piece');
 
 // NOTE: Your files can have multiple module.exports statements!
 // Make sure to export both your Museum and Piece models!
