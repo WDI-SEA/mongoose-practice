6fd6118c791b3bef9d907701249bac3c9677e643
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-09-11 08:39:54 -0700

    added schemas and exports

--- models/museum.js
+++ models/museum.js
@@ -2,7 +2,12 @@
 const mongoose = require('mongoose');
 
 // TODO: Create Museum Schema
+const museumSchema = new mongoose.Schema({
+	name: String,
+	city: String,
+	country: String,
+	image: String
+});
 
-// TODO: Use schema to create model
-
-// TODO: Export Museum Model
+// TODO: Use schema to create model and Export Museum Model
+module.exports = mongoose.model('Museum', museumSchema);
