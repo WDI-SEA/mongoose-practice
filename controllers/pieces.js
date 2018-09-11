0155c610b4e76e48b04ae06167ba8896ee4f759c
Sky <anvandp18@gmail.com>
2018-09-11 10:35:19 -0700

    update

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -24,12 +24,38 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.send('STUB - NEW PIECES POST');
+ db.Piece.create({
+  name: req.body.name,
+  image: req.body.image,
+  originCountry: req.body.originCountry,
+  museum: req.body.museum,
+  creator: {
+    firstname: req.body.creator_firstname,
+    lastname: req.body.creator_lastname,
+    image: req.body.creator_image,
+    birthyear: req.body.creator_birthyear,
+    deathyear: req.body.creator_deathyear
+  }
+ })
+ .then(result => {
+    res.redirect(`/pieces/${result.id}`)
+ })
+ .catch(err => {
+    console.log(err);
+    res.render('error');
+  });
 });
 
 router.get('/new', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.render('pieces/new');
+  db.Museum.find()
+  .then(museum => {
+     res.render('pieces/new', { museums: museums });
+  })
+  .catch(err => {
+    console.log(err);
+    res.render('error');
+  });
 });
 
 router.get('/:id', (req, res) => {
