7562c2eb307e14f6d4595e8f99e477e6ef73da30
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-09-13 13:34:23 -0700

    most of assignment completed

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -9,23 +9,59 @@ const db = require('../models');
 
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
-  res.render('pieces/index');
+  db.Piece.find()
+  	.populate('museum')
+  	.then( pieces => {
+  		console.log('The pieces are: ', pieces);
+  		res.render('pieces/index', { pieces: pieces });
+  	}).catch( err => {
+  		console.log(err);
+  		res.send(err);
+  	})
 });
 
 router.post('/', (req, res) => {
-  // TODO: Replace stub route with page that renders form for adding new piece
+  db.Piece.create({
+  	name: req.body.name,
+  	image: req.body.image,
+  	museum: req.body.museum,
+  	creator: {
+  		firstename: req.body.firstname,
+  		lastname: req.body.lastname,
+  		image: req.body.creator_image,
+  		birthyear: req.body.birthyear,
+  		deathyear: req.body.deathyear
+  	}
+  }).then( piece => {
+  		console.log('NEW PIECE!');
+  		res.redirect(`/pieces/${piece.id}`);
+  	}).catch( err => {
+  		console.log(err);
+  		res.send(err);
+  	})
   res.send('STUB - NEW PIECES POST');
 });
 
 router.get('/new', (req, res) => {
-  // TODO: Replace stub route with page that renders form for adding new piece
-  res.render('pieces/new');
+  db.Museum.find()
+  	.then(museums => {
+  		console.log('Found Museums: ', museums)
+  		res.render('pieces/new', { buildings: museums })
+  	}).catch(err => {
+  		console.log(err);
+  		res.send(err);
+  	})
 });
 
 router.get('/:id', (req, res) => {
-  // TODO: Replace stub route with page that renders piece details
-  //  and all the info about it's creator and the museum it's located in
-  res.send('pieces/show');
+  db.Piece.findById(req.params.id)
+  .populate('museum')
+  	.then(result => {
+  		res.render('pieces/show', { piece: result })
+  	}).catch(err => {
+  		console.log(err);
+  		res.send(err);
+  	})
 });
 
 module.exports = router;
