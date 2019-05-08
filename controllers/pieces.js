23acb9cadf073a6ff77fd073d60d5c2367f2fb3f
JustinRobertKing <kingj4@spu.edu>
2019-05-07 18:59:09 -0700

    pieces list working

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -4,36 +4,50 @@ const express = require('express');
 // Declare router
 const router = express.Router();
 
+let db = require('../models')
+
+
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
   db.Piece.find()
   .populate('museum')
   .then((pieces) => {
-    res.render('pieces/index', {pieces});
+    res.render('pieces/index', {pieces})
+    // res.render('pieces/index', {pieces});
+  })
+  .catch((error) => {
+    console.log('error', error)
   })
 });
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
   db.Piece.create({
-  	name: req.body.name,
-  	image: req.body.image,
-  	originCountry: req.body.originCountry,
-  	museum: req.body.museum,
-  	creator: {
-  		firstname: req.body.creator_firstname,
-  		lastname: req.body.creator_lastname,
-  		image: req.body.creator_image,
-  		birthyear: req.body.creator_birthyear,
-  		deathyear: req.body.creator_deathyear
-  	}
+    name: req.body.name,
+    image: req.body.image,
+    museum: req.body.museum,
+    creator: {
+      firstname: req.body.creator_firstname,
+      lastname: req.body.creator_lastname,
+      image: req.body.creator_image,
+      birthyear: req.body.creator_birthyear,
+      deathyear: req.body.creator_deathyear
+    }
   })
-  res.send('STUB - NEW PIECES POST');
+  .then(newMuseum => {
+    res.redirect('/pieces');
+  })
 });
 
 router.get('/new', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.render('pieces/new');
+  db.Museum.find()
+  .then(museums => {
+    res.render('pieces/new', {museums});
+  })
+  .catch((error) => {
+    console.log('error', error)
+  })
 });
 
 router.get('/:id', (req, res) => {
