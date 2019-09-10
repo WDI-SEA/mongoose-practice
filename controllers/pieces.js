95df877c4a135c5c196d38c03d068eaad96bcd73
michelleurrea <michelleurrea@gmail.com>
2019-09-10 11:02:38 -0700

    Homework review in class with Henry

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -4,24 +4,64 @@ const express = require('express');
 // Declare router
 const router = express.Router();
 
+const db = require('../models')
+
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
-  res.render('pieces/index');
+  db.Piece.find()
+  .populate('muesum')
+  .then(pieces => {
+  	res.render('pieces/index', { pieces })
+  })
+  .catch(err => {
+  	console.log('Error', err)
+  	res.render('error', {
+  		message: 'Error in GET /pieces'
+  	})
+  })
 });
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.send('STUB - NEW PIECES POST');
+  db.Piece.create({
+  	name: req.body.name,
+  	image: req.body.image,
+  	museum: req.body.museum,
+  	creator: {
+  		firstname: req.body.creator_firstname,
+  		lastname: req.body.creator_lastname,
+  		image: req.body.creator_image,
+  		birthyear: req.body.creator_birthyear,
+  		deathyear: req.body.creator_deathyear
+  	}
+})
+  .then(result => {
+  	res.redirect('/pieces/' + result.id)
+  })
+   .catch(err => {
+  	console.log('Error', err)
+  	res.render('error', {
+  		message: 'Error in GET /pieces'
+  	})
+  })
 });
 
 router.get('/new', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.render('pieces/new');
+  db.Museum.find()
+  .select({ name: 1 })
+  .exec()
+  .then(museums => {
+  	res.render('pieces/new', {museums});
+  })
 });
 
 router.get('/:id', (req, res) => {
   // TODO: Replace stub route with page that renders piece details
   //  and all the info about it's creator and the museum it's located in
+  db.Piece.findById(req.params.id)
+  .populate('museum')
+  .then(piece)
   res.send('pieces/show');
 });
 
