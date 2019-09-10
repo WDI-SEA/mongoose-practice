183ff2d7a09d61e96f373f11541634280d1abc8b
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-09-10 10:56:54 -0700

    piece post

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
   db.Piece.find()
+  .populate('museum')
   .then(pieces => {
     res.render('pieces/index', {
       pieces
@@ -21,29 +22,43 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  db.Piece.create(req.body)
-  .then(() => {
-    res.redirect('/pieces');
+  db.Piece.create({
+    name: req.body.name,
+    image: req.body.image,
+    museum: req.body.museum,
+    creator: {
+      firstname: req.body.creator_firstname,
+      lastname: req.body.creator_lastname,
+      image: req.body.creator_image,
+      birthyear: req.body.creator_birthyear,
+      deathyear: req.body.creator_deathyear,
     }
-  )
-  .catch(err => {
-    console.log(err)
+  })
+
+  .then(result => {
+    res.redirect('/pieces/' + result.id);
+    })
+    .catch(err => {
+      console.log(err)
   })
 });
 
 router.get('/new', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
   db.Museum.find()
+  .select({name: 1}) 
+  .exec()
   .then(museums => {
     res.render('pieces/new', {
       museums
-      }
-    );
-  })
-  .catch(err => {
-    console.log(err);
-  })
-});
+      })
+    }) 
+    .catch(err => {
+      console.log(err);
+    })
+  }
+)
+
 
 router.get('/:id', (req, res) => {
   // TODO: Replace stub route with page that renders piece details
