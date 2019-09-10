6e9861384b299842abf832b97ea4f78a4af436f3
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2019-09-10 05:52:21 -0700

    created models

--- models/museum.js
+++ models/museum.js
@@ -2,7 +2,24 @@
 const mongoose = require('mongoose');
 
 // TODO: Create Museum Schema
-
 // TODO: Use schema to create model
+let museumSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    image: String,
+})
 
 // TODO: Export Museum Model
+module.exports = mongoose.model('Museum', museumSchema)
+
+
--- models/piece.js
+++ models/piece.js
@@ -1,14 +1,30 @@
 // Require Mongoose node module
 const mongoose = require('mongoose');
+const Object_Id = mongoose.Schema.Types.ObjectId;
 
 // TODO: Create Creator Schema
+let creatorSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    image: String,
+    birthyear: Number,
+    deathyear: Number,
+    museum: Object_Id
+  })
 
 // TODO: Create Piece Schema
 // HINT: include a creator field for using the Creator schema
+let pieceSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    creator: creatorSchema
+  })
+  
 
 // TODO: Use Piece schema to create Piece model
 
 // TODO: Export Piece Model
+module.exports = mongoose.model('Piece', pieceSchema)
 
 
 // NOTE: You don't need to worry about Creator schema. You don't need to
