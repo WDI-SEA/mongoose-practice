15045a07d8cfb2e8cba55c88bf53a60de87df2ca
larynxmith <larynxmith@gmail.com>
2019-09-10 01:06:00 -0700

    Pieces routes created, /pieces functional

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -1,17 +1,47 @@
 // Require needed modules
 const express = require('express');
+let db = require('../models')
 
 // Declare router
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
-  res.render('pieces/index');
+  db.Piece.find()
+  .populate('museum')
+  .then(pieces => {
+      res.render('pieces/index', { pieces });
+      
+  })
+  .catch(err => {
+      console.log(err)
+      res.send("Error in GET /pieces route")
+})
 });
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.send('STUB - NEW PIECES POST');
+  db.Piece.create({
+      name: req.body.name,
+      image: req.body.image,
+      museum: req.body.museum,
+      creator: {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        image: req.body.creatorimage,
+        birthyear: req.body.birthyear,
+        deathyear: req.body.deathyear
+        
+      }
+  })
+  .then(createdPiece => {
+      console.log('New Piece Added: ', createdPiece)
+      res.redirect('/pieces')
+  })
+  .catch(err => {
+      console.log(err)
+      res.send('Error in adding New Piece')
+  })
 });
 
 router.get('/new', (req, res) => {
@@ -22,7 +52,14 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   // TODO: Replace stub route with page that renders piece details
   //  and all the info about it's creator and the museum it's located in
-  res.send('pieces/show');
+  db.Piece.findById(req.params.id)
+  .then(foundPiece => [
+      res.render('pieces/show', { foundPiece })
+  ])
+  .catch(err => {
+      console.log(err)
+      res.send('Error in GET /pieces/show route')
+  })
 });
 
 module.exports = router;
