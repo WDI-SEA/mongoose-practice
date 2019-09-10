660110b9652947afb22e874aa22a051db39370bb
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-09-10 10:06:25 -0700

    adding piece logic

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -21,7 +21,14 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.send('STUB - NEW PIECES POST');
+  db.Museum.create(req.body)
+  .then(() => {
+    res.redirect('/pieces');
+    }
+  )
+  .catch(err => {
+    console.log(err)
+  })
 });
 
 router.get('/new', (req, res) => {
@@ -32,7 +39,10 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   // TODO: Replace stub route with page that renders piece details
   //  and all the info about it's creator and the museum it's located in
-  res.send('pieces/show');
+  db.Piece.find(req.params.id)
+  .then(result => {
+    res.render('pieces/show')
+  })
 });
 
 module.exports = router;
