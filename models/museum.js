214b1d94d8bfc7720b7051c4c86ce57995d378d8
RebeccaEvans <evans.rebeccaj@gmail.com>
2020-01-23 08:35:27 -0800

    initial models and files

--- models/museum.js
+++ models/museum.js
@@ -6,3 +6,40 @@ const mongoose = require('mongoose');
 // TODO: Use schema to create model
 
 // TODO: Export Museum Model
+
+
+	//create schema
+	var museumSchema = new mongoose.Schema({
+		name: String,
+		city: String,
+		country: String,
+		imageUrl: String
+	  })
+
+	//create model
+	var Museum = mongoose.model('Museum', museumSchema)
+
+	//create museum documents
+	var sam = new Museum({ 
+		name: 'SAM', 
+		city: Seattle, 
+		country: USA, 
+		imageUrl: http://www.seattleartmuseum.org/Assets%20Visit/seattle-art-museum/seattle-art-museum-header-780px.jpg
+		})
+
+	//save documents to db
+	sam.save(function (err, sam) {
+		if (err) return console.error(err)
+		console.log('Museum Saved')
+	  })
+
+
+	//display all documents
+	Museum.find(function (err, museums) {
+		if (err) return console.error(err);
+		console.log(museums);
+	  })
+
+})
+
+module.exports = museum;
--- models/piece.js
+++ models/piece.js
@@ -14,3 +14,47 @@ const mongoose = require('mongoose');
 // NOTE: You don't need to worry about Creator schema. You don't need to
 // create a model for it or export it. This is because it lives inside
 // the Piece model, so that takes care of it all! Yay for embedded schemas!
+
+	//create schemas
+	var creatorSchema = new mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		imageUrl: String,
+		birthYear: String,
+		deathYear: String
+	  })
+
+
+	var pieceSchema = new mongoose.Schema({
+		name: String,
+		imageUrl: String,
+		creator: []
+	  })
+
+	//create model
+	var Piece = mongoose.model('Piece', pieceSchema)
+
+	//create documents
+	var sam = new Museum({ 
+		name: 'SAM', 
+		city: Seattle, 
+		country: USA, 
+		imageUrl: http://www.seattleartmuseum.org/Assets%20Visit/seattle-art-museum/seattle-art-museum-header-780px.jpg
+		})
+
+	//save documents to db
+	sam.save(function (err, sam) {
+		if (err) return console.error(err)
+		console.log('Museum Saved')
+	  })
+
+
+	//display all documents
+	Museum.find(function (err, kittens) {
+		if (err) return console.error(err);
+		console.log(kittens);
+	  })
+
+})
+
+module.exports = router;
