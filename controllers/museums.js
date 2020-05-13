6e3a3121d4321b42a120120ef484fde49031a05d
garedb <garedbarney@gmail.com>
2020-05-13 15:58:28 -0700

    homework

--- controllers/museums.js
+++ controllers/museums.js
@@ -1,28 +1,58 @@
 // Require needed modules
 const express = require('express');
+let db = require('../models')
 
 // Declare router
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // TODO: Replace stub route with page that renders list of all museums
-  res.render('museums/index');
+  db.Museum.find()
+  .then(museums => {
+    res.render('museums/index', {museums});
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('Error')
+  })
 });
 
 router.post('/', (req, res) => {
-  // TODO: Replace stub route with page that renders form for adding new museum
-  res.send('STUB - NEW MUSEUM POST');
+  db.Museum.create({
+    name: req.body.name,
+    city: req.body.city,
+    country: req.body.country,
+    image: req.body.image
+  })
+  .then(newMuseum => {
+    console.log(newMuseum)
+    res.redirect('/museums')
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('Error')
+  })
 });
 
 router.get('/new', (req, res) => {
-  // TODO: Replace stub route with page that renders form for adding new museum
   res.render('museums/new');
 });
 
 router.get('/:id', (req, res) => {
-  // TODO: Replace stub route with page that renders museum details
-  //  and a list of pieces that musuem contains
-  res.send('museums/show');
+  db.Museum.findOne({_id: req.params.id})
+  .then(museumData => {
+    db.Piece.find({museum: req.params.id})
+    .then(pieces => {
+      res.render('museums/show', {museum: museumData, pieces})
+    })
+    .catch(err => {
+      console.log(err)
+      res.send('Error')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.send('Error')
+  })
 });
 
 module.exports = router;
--- models/piece.js
+++ models/piece.js
@@ -1,14 +1,33 @@
 // Require Mongoose node module
 const mongoose = require('mongoose');
 
-// TODO: Create Creator Schema
+//Create Creator Schema
+const creatorSchema = new mongoose.Schema({
+    firstname: {
+        type: String, required: true
+    },
+    lastname: {
+        type: String, required: true
+    },
+    image: String,
+    birthYear: Date,
+    deathYear: Date
+    }
+)
 
-// TODO: Create Piece Schema
-// HINT: include a creator field for using the Creator schema
+//Create Piece Schema
+const pieceSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true
+    },
+    image: String,
+    museum: {type: mongoose.Schema.Types.ObjectId, ref: 'Museum'},
+    creator: [creatorSchema]
+    }
+)
 
-// TODO: Use Piece schema to create Piece model
-
-// TODO: Export Piece Model
+//Use Piece schema to create Piece model & export
+module.exports = mongoose.model('Piece', pieceSchema)
 
 
 // NOTE: You don't need to worry about Creator schema. You don't need to
